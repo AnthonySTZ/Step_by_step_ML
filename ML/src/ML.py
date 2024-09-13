dc3056102da450445c95fb89aa6243dfa92c6bcb
AnthonySTZ <anthonycandy2309@gmail.com>
2024-09-13 10:35:58 +0200

    init ML.py

---
+++ ML/src/ML.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+def load_dataset(url: str, names: list[str]) -> pd.DataFrame:
+    """Loads a dataset from the given URL"""
+    dataset = read_csv(url, names=names)
+    return dataset
