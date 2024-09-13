f06dcfed27b2563922fd689954646694d103e474
AnthonySTZ <anthonycandy2309@gmail.com>
2024-09-13 14:57:33 +0200

    split out validation dataset

--- ML/src/ML.py
+++ ML/src/ML.py
@@ -45,3 +45,15 @@ def show_multivariate_plot(dataset: pd.DataFrame) -> None:
     """Plot Multivariate Dataset"""
     scatter_matrix(dataset)
     plt.show()
+
+
+def split_out_validation_dataset(dataset: pd.DataFrame) -> tuple:
+
+    array = dataset.values
+    X = array[:, 0:4]
+    y = array[:, 4]
+    X_train, X_validation, Y_train, Y_validation = train_test_split(
+        X, y, test_size=0.20, random_state=1
+    )
+
+    return X_train, X_validation, Y_train, Y_validation
--- ML/src/main.py
+++ ML/src/main.py
@@ -8,7 +8,10 @@ def run():
     # ML.show_some_dataset_info(dataset)
     # ML.show_univariate_plot(dataset)
     # ML.show_histogram(dataset)
-    ML.show_multivariate_plot(dataset)
+    # ML.show_multivariate_plot(dataset)
+    X_train, X_validation, Y_train, Y_validation = ML.split_out_validation_dataset(
+        dataset
+    )
 
 
 if __name__ == "__main__":
